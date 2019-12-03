54bb5481d8097c417c4fe21dc0b9f6ac35f50f60
Rubén Alba <ruben.alba@birchmangroup.com>
2019-12-03 12:48:12 +0100

    sobreescrito

---
+++ CollectionView/CollectionView.Android/obj/Release/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 6b2c2ef0680d83af213073504f4084567ccb58d9 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1030
+	/* entry-length */
+	.word	238
+	/* value-offset */
+	.word	136
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 245141
+	.include	"typemap.mj.inc"
